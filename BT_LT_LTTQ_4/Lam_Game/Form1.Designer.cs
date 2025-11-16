eef6ab58b983347295fac361a24ac22d162790c8
ohlavienn <trivien.do@gmail.com>
2025-11-16 13:42:16 +0700

    Add files via upload

---
+++ BT_LT_LTTQ_4/Lam_Game/Form1.Designer.cs
@@ -0,0 +1,29 @@
+﻿namespace SlimeAnimation
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Slime Animation";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
+        }
+
+        #endregion
+    }
+}
---
+++ BT_LT_LTTQ_4/Lam_Game/Program.cs
@@ -0,0 +1,17 @@
+﻿using Lam_Game;
+using System;
+using System.Windows.Forms;
+
+namespace SlimeAnimation
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
